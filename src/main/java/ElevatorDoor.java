f4bed3e462302a185ebff631a37a6e2abf529269
Monolong <83206119+Monolong@users.noreply.github.com>
2024-05-26 20:47:56 +0900

    Revert "ElevatorDoor 클래스 단순화"
    This reverts commit 2df59544caa2492970331de0b03a417e690c0ad9.

--- src/main/java/ElevatorDoor.java
+++ src/main/java/ElevatorDoor.java
@@ -1,25 +1,77 @@
-public class ElevatorDoor 
-{
-	private DeviceVendor vendor;
-	private DoorStatus elevatorDoorStatus;
 
-	public ElevatorDoor(DeviceVendor vendor) 
-	{
+public class ElevatorDoor {
+	private DeviceVendor vendor;
+
+	private DoorStatus elevatorDoorStatusForLG ;
+	private DoorStatus elevatorDoorStatusForSamsung ;
+	private DoorStatus elevatorDoorOpenedForHyundai ;
+
+	public ElevatorDoor(DeviceVendor vendor) {
 		this.vendor = vendor;
-		elevatorDoorStatus = DoorStatus.CLOSED;
+		
+		switch ( vendor ) {
+		case LG:
+			elevatorDoorStatusForLG = DoorStatus.CLOSED;
+			break;
+		case HYUNDAI:
+			elevatorDoorOpenedForHyundai = DoorStatus.CLOSED;
+			break;
+		case SAMSUNG:
+			elevatorDoorStatusForSamsung = DoorStatus.CLOSED;
+			break;
 		}
+	}
 	public DoorStatus getDoorStatus() {
 		DoorStatus status = null;
-		status = elevatorDoorStatus == DoorStatus.CLOSED ? DoorStatus.CLOSED : DoorStatus.OPEN;
+		switch ( vendor ) {
+		case LG:
+			status = elevatorDoorStatusForLG.equals(DoorStatus.CLOSED) ? DoorStatus.CLOSED : DoorStatus.OPEN;
+			break;
+		case HYUNDAI:
+			status = elevatorDoorOpenedForHyundai == DoorStatus.CLOSED ? DoorStatus.CLOSED : DoorStatus.OPEN;
+			break;
+		case SAMSUNG:
+			status = elevatorDoorStatusForSamsung == DoorStatus.CLOSED ? DoorStatus.CLOSED : DoorStatus.OPEN;
+			break;
+		}
 		return status;
 	}
-
-	public void open() 
-	{
-		elevatorDoorStatus = DoorStatus.OPEN;
+	public void open() {
+		switch ( vendor ) {
+		case LG:
+			if ( elevatorDoorStatusForLG.equals(DoorStatus.OPEN) ) return ;
+			System.out.println("open LG Elevator Door") ;
+			elevatorDoorStatusForLG = DoorStatus.CLOSED;
+			break;
+		case HYUNDAI:
+			if ( elevatorDoorOpenedForHyundai == DoorStatus.CLOSED ) return ;
+			System.out.println("open Hyundai Elevator Door") ;
+			elevatorDoorOpenedForHyundai = DoorStatus.CLOSED;
+			break;
+		case SAMSUNG:
+			if ( elevatorDoorStatusForSamsung == DoorStatus.CLOSED ) return ;
+			System.out.println("open Samsung Elevator Door") ;
+			elevatorDoorStatusForSamsung = DoorStatus.CLOSED;
+			break;
+		}	
 	}
-	public void close() 
-	{
-		elevatorDoorStatus = DoorStatus.CLOSED;
+	public void close() {
+		switch ( vendor ) {
+		case LG:
+			if ( elevatorDoorStatusForLG.equals(DoorStatus.CLOSED) ) return ;
+			System.out.println("close LG Elevator Door") ;		
+			elevatorDoorStatusForLG = DoorStatus.CLOSED;
+			break;
+		case HYUNDAI:
+			if ( elevatorDoorOpenedForHyundai == DoorStatus.CLOSED  ) return ;
+			System.out.println("close Hyundai Elevator Door") ;		
+			elevatorDoorOpenedForHyundai = DoorStatus.CLOSED;
+			break;
+		case SAMSUNG:
+			if ( elevatorDoorStatusForSamsung == DoorStatus.CLOSED  ) return ;
+			System.out.println("close Samsung Elevator Door") ;		
+			elevatorDoorStatusForSamsung = DoorStatus.CLOSED;
+			break;
+		}
 	}
 }
